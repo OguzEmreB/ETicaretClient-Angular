5dad368e150765ac485ef3ee6c89528f98be4d74
OguzEmre <oguzemre.b@gmail.com>
2024-05-22 00:44:58 +0300

    Authentication

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClientService } from '../http-client.service';
-import { access } from 'fs';
-import { Action } from 'rxjs/internal/scheduler/Action';
+import { HttpClientService } from '../http-client.service'; 
 import { User } from '../../../entities/user';
 import { Create_User } from '../../../contracts/users/create_user';
 import { Observable, firstValueFrom } from 'rxjs';
+import { Token } from '../../../contracts/token/token'; 
+import { CustomToastrService, ToasterMessageType, ToasterPosition } from '../../ui/custom-toastr.service';
+import { TokenResponse } from '../../../contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService:HttpClientService) { }
+  constructor(private httpClientService:HttpClientService, private toastrSerVice:CustomToastrService) { }
 
   async create(user:User) : Promise<Create_User>{
    const observable : Observable<Create_User | User> =  this.httpClientService.post<Create_User | User>({
       controller:"users",
     },user);
    return await firstValueFrom(observable) as Create_User;
+  }
+
+  async login(userNameOrEmail:string,password:string, callBackFunction?:() => void):Promise<any>{
+  const observable:Observable<any | TokenResponse>=   this.httpClientService.post<any | TokenResponse>({
+      controller:"users",
+      action:"login"
+    },{userNameOrEmail,password})
+   const tokenResponse : TokenResponse =  await firstValueFrom(observable) as TokenResponse;
+
+   if(tokenResponse){
+
+    localStorage.setItem("accessToken",tokenResponse.token.accessToken);
+this.toastrSerVice.message("Kullanıcı girişi başarıyla sağlanmıştır.","Giriş Başarılı",{
+  messageType:ToasterMessageType.Success,
+  position:ToasterPosition.TopRight
+})
+   }
+    callBackFunction();
   }
 }
