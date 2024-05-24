058274c6e8f7e3d0c805dbb6ad90589a04cedd99
OguzEmre <oguzemre.b@gmail.com>
2024-05-24 22:43:15 +0300

    Http Interceptor - Error Handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
-import { UiModule } from './ui/ui.module'; 
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations'; 
+import { UiModule } from './ui/ui.module';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { NgxSpinnerModule } from 'ngx-spinner'; 
-import { HttpClientModule } from '@angular/common/http';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
-import { FacebookLoginProvider, GoogleLoginProvider,   GoogleSigninButtonModule,   SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login'; 
+import { FacebookLoginProvider, GoogleLoginProvider, GoogleSigninButtonModule, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
+import { HttpErrorHandlerInterceptorService } from './services/common/http-error-handler-interceptor.service';
 
 
 @NgModule({
@@ -27,35 +28,36 @@ import { FacebookLoginProvider, GoogleLoginProvider,   GoogleSigninButtonModule,
     NgxSpinnerModule,
     HttpClientModule,
     JwtModule.forRoot({
-      config:{
-        tokenGetter:()=> localStorage.getItem("accessToken"),
-        allowedDomains:["localhost:7151"]
+      config: {
+        tokenGetter: () => localStorage.getItem("accessToken"),
+        allowedDomains: ["localhost:7151"]
       }
     }),
-    SocialLoginModule,  GoogleSigninButtonModule
+    SocialLoginModule, GoogleSigninButtonModule
   ],
   providers: [
-  {provide:"baseUrl",useValue:"https://localhost:7151/api",multi:true},
+    { provide: "baseUrl", useValue: "https://localhost:7151/api", multi: true },
     provideAnimations(),
     {
       provide: "SocialAuthServiceConfig",
-    useValue: {
-      autoLogin: false,
-      providers: [
-        {
-          id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider("951868154971-l572msahn2qv63vqvuibge60lq3hr94c.apps.googleusercontent.com")
-        },
-        {
-          id:FacebookLoginProvider.PROVIDER_ID,
-          provider:new FacebookLoginProvider("")
-        }
-      ],
-      onError: err => console.log(err)
-    } as SocialAuthServiceConfig
-  
- }
-],
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider("951868154971-l572msahn2qv63vqvuibge60lq3hr94c.apps.googleusercontent.com")
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider("")
+          }
+        ],
+        onError: err => console.log(err)
+      } as SocialAuthServiceConfig
+    },{
+      provide:HTTP_INTERCEPTORS, useClass:HttpErrorHandlerInterceptorService, multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 
---
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -0,0 +1,50 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { error } from 'console';
+import { Observable, catchError, of } from 'rxjs';
+import { CustomToastrService, ToasterMessageType, ToasterPosition } from '../ui/custom-toastr.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
+
+  constructor(private toastrService: CustomToastrService) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(error => {
+      switch (error.status) {
+        case HttpStatusCode.Unauthorized:
+          this.toastrService.message("Giriş Reddedildi.", "Yetkisiz İşlem", {
+            messageType: ToasterMessageType.Error,
+            position: ToasterPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.InternalServerError:
+          this.toastrService.message("Sunucuya Erişilemiyor.", "Sunucu Hatası", {
+            messageType: ToasterMessageType.Error,
+            position: ToasterPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.BadRequest:
+          this.toastrService.message("Geçersiz İstek Yapıldı", "Geçersiz İstek", {
+            messageType: ToasterMessageType.Error,
+            position: ToasterPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.NotFound:
+          this.toastrService.message("Sayfa Bulunamadı.", "Sayfa Bulunamadı.", {
+            messageType: ToasterMessageType.Error,
+            position: ToasterPosition.BottomFullWidth
+          });
+          break;
+        default:
+          this.toastrService.message("Beklenmeyen Bir Hata Meydana Geldi", "Hata!", {
+            messageType: ToasterMessageType.Error,
+            position: ToasterPosition.BottomFullWidth
+          });
+          break;
+      }
+      return of(error);
+    }));
+  }
+}
