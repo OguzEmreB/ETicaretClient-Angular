ada2417ad00fb059e8dc19cc384169d6174b156d
OguzEmre <oguzemre.b@gmail.com>
2024-05-25 01:44:55 +0300

    RefreshToken

--- src/app/services/common/http-error-handler-interceptor.service.ts
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -1,15 +1,15 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { error } from 'console';
 import { Observable, catchError, of } from 'rxjs';
 import { CustomToastrService, ToasterMessageType, ToasterPosition } from '../ui/custom-toastr.service';
+import { UserAuthService } from './models/user-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
 
-  constructor(private toastrService: CustomToastrService) { }
+  constructor(private toastrService: CustomToastrService, private userAuthService: UserAuthService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(catchError(error => {
       switch (error.status) {
@@ -17,6 +17,10 @@ export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
           this.toastrService.message("Giriş Reddedildi.", "Yetkisiz İşlem", {
             messageType: ToasterMessageType.Error,
             position: ToasterPosition.BottomFullWidth
+          });
+
+          this.userAuthService.refreshTokenLogin(localStorage.getItem("refreshToken")).then(data=>{
+            
           });
           break;
         case HttpStatusCode.InternalServerError:
--- src/app/services/common/models/user-auth.service.ts
+++ src/app/services/common/models/user-auth.service.ts
@@ -10,43 +10,55 @@ import { SocialUser } from '@abacritt/angularx-social-login';
 })
 export class UserAuthService {
 
-  constructor(private httpClientService:HttpClientService, private toastrSerVice:CustomToastrService) { }
+  constructor(private httpClientService: HttpClientService, private toastrSerVice: CustomToastrService) { }
 
 
-  async login(userNameOrEmail:string,password:string, callBackFunction?:() => void):Promise<any>{
-    const observable:Observable<any | TokenResponse>=   this.httpClientService.post<any | TokenResponse>({
-        controller:"auth",
-        action:"login"
-      },{userNameOrEmail,password})
-     const tokenResponse : TokenResponse =  await firstValueFrom(observable) as TokenResponse;
-  
-     if(tokenResponse){
-  
-      localStorage.setItem("accessToken",tokenResponse.token.accessToken);
-  this.toastrSerVice.message("Kullanıcı girişi başarıyla sağlanmıştır.","Giriş Başarılı",{
-    messageType:ToasterMessageType.Success,
-    position:ToasterPosition.TopRight
-  })
-     }
-      callBackFunction();
+  async login(userNameOrEmail: string, password: string, callBackFunction?: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
+      controller: "auth",
+      action: "login"
+    }, { userNameOrEmail, password })
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+      localStorage.setItem("refreshToken", tokenResponse.token.refreshToken);
+      this.toastrSerVice.message("Kullanıcı girişi başarıyla sağlanmıştır.", "Giriş Başarılı", {
+        messageType: ToasterMessageType.Success,
+        position: ToasterPosition.TopRight
+      })
     }
-  
-    async googleLogin(user:SocialUser,callBackFunction?:()=>void):Promise<any>{
-      const observable: Observable<SocialUser| TokenResponse> = this.httpClientService.post({
-        action:"google-login",
-        controller:"auth"
-      },user);
-  
-      const tokenResponse:TokenResponse= await firstValueFrom(observable) as TokenResponse;
-  
-      if(tokenResponse){
-        localStorage.setItem("accessToken",tokenResponse.token.accessToken);
-        this.toastrSerVice.message("Google girişi başarılı","Giriş Başarılı",{
-          messageType:ToasterMessageType.Success,
-          position:ToasterPosition.TopRight
-        });
-      }
-      callBackFunction();
+    callBackFunction();
+  }
+  async refreshTokenLogin(refreshToken: string, callBackFunction?: () => void):Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post({
+      action: "refreshTokenLogin",
+      controller: "auth"
+    }, { refreshToken: refreshToken });
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+      localStorage.setItem("refreshToken", tokenResponse.token.refreshToken);
     }
+    callBackFunction();
+  }
+  async googleLogin(user: SocialUser, callBackFunction?: () => void): Promise<any> {
+    const observable: Observable<SocialUser | TokenResponse> = this.httpClientService.post({
+      action: "google-login",
+      controller: "auth"
+    }, user);
+
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+      localStorage.setItem("refreshToken", tokenResponse.token.refreshToken);
+      this.toastrSerVice.message("Google girişi başarılı", "Giriş Başarılı", {
+        messageType: ToasterMessageType.Success,
+        position: ToasterPosition.TopRight
+      });
+    }
+    callBackFunction();
+  }
 
 }
